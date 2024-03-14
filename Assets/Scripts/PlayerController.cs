01b06963a97588217d8c6df743104dad713efdb9
incognitosushiroll <157520631+incognitosushiroll@users.noreply.github.com>
2024-03-14 15:07:28 -0700

    OOP into PlayerController+Weapons, started unit tests

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,83 +1,174 @@
 using System.Collections;
 using System.Collections.Generic;
-//using System.Numerics; commented out due to Vector2 conflicts with InputSystem pkg
 using UnityEngine;
 using UnityEngine.InputSystem;
+//using Weaponspace; 
 
-public class PlayerController : MonoBehaviour
+namespace Playerspace
 {
-    //For movement; done by Matt 
-    public float moveSpeed = 1f;
-    public float collisionOffset = 0.05f;
-    public ContactFilter2D movementFilter;
-    Vector2 movementInput;
-    Rigidbody2D rb; //also used for shooting ammo
-    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
-
-    //For Shooting hot dogs; done by K 
-    public Weapon weapon; 
-    Vector2 moveDirection; 
-    Vector2 mousePosition; 
-    //private bool isFiring = false; //Tracks whether firing is in progress 
-
-    // Start is called before the first frame update
-    void Start()
+    public class PlayerController : MonoBehaviour
     {
-        rb = GetComponent<Rigidbody2D>();
-        
-    }
+        //For movement
+        public float moveSpeed = 1f;
+        public float collisionOffset = 0.05f;
+        public ContactFilter2D movementFilter;
+        public Vector2 movementInput;
+        Rigidbody2D rb;
+        List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
-    //K's section for shooting ammo 
+        //For shooting
+        //public Weaponspace.Weapon weapon = new Weaponspace.Weapon();
+        public Weapon weapon; 
+        Vector2 moveDirection;
+        Vector2 mousePosition;
 
-    public bool isFiring = false; 
-
-    public void StartFiring(){ 
-        isFiring = true; 
-        StartCoroutine(FireCoroutine()); 
-    }
-
-    public void StopFiring() { 
-        isFiring = false; 
-    }
-
-    IEnumerator FireCoroutine() { 
-        while (isFiring) { 
-            weapon.Fire(); 
-            yield return new WaitForSeconds(0.3f); //Adjust the delay between shots 
+        void Start()
+        {
+            rb = GetComponent<Rigidbody2D>();
         }
-    }
-    void Update() { 
-        float moveX = Input.GetAxisRaw("Horizontal"); 
-        float moveY = Input.GetAxisRaw("Vertical"); 
 
-        if(Input.GetKeyDown(KeyCode.Space)) {
-            StartFiring(); 
-        }
-        else if (Input.GetKeyUp(KeyCode.Space)) { 
-            StopFiring(); 
-        }
-        moveDirection = new Vector2(moveX, moveY).normalized; 
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
-    }
+        public bool isFiring = false;
 
-    private void FixedUpdate() 
-    {
-        // if movement input is not 0, try to move - Matt
-        if (movementInput != Vector2.zero) {
-            int count = rb.Cast(movementInput, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
-            if (count == 0) {
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+        public void StartFiring(MonoBehaviour monoBehaviour)
+        {
+            isFiring = true;
+            monoBehaviour.StartCoroutine(FireCoroutine());
+        }
+
+        public void StopFiring()
+        {
+            isFiring = false;
+        }
+
+        IEnumerator FireCoroutine()
+        {
+            while (isFiring)
+            {
+                weapon.Fire();
+                yield return new WaitForSeconds(0.3f); //Adjust the delay between shots 
             }
-        }  
+        }
 
-        // handle physics calcs for shooting - K 
-        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed); 
-        Vector2 aimDirection = mousePosition - rb.position; //Calculating angle based on mouse pos
-        float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f; 
-        rb.rotation = aimAngle; 
-    }
+        void Update()
+        {
+            float moveX = Input.GetAxisRaw("Horizontal");
+            float moveY = Input.GetAxisRaw("Vertical");
 
-    void OnMove(InputValue movementValue) {
-        movementInput = movementValue.Get<Vector2>();
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                StartFiring(this);
+            }
+            else if (Input.GetKeyUp(KeyCode.Space))
+            {
+                StopFiring();
+            }
+            moveDirection = new Vector2(moveX, moveY).normalized;
+            mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        void FixedUpdate()
+        {
+            if (movementInput != Vector2.zero)
+            {
+                int count = rb.Cast(movementInput, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+                if (count == 0)
+                {
+                    rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+                }
+            }
+
+            rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+            Vector2 aimDirection = mousePosition - rb.position;
+            float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f;
+            rb.rotation = aimAngle;
+        }
+
+        void OnMove(InputValue movementValue)
+        {
+            movementInput = movementValue.Get<Vector2>();
+        }
     }
 }
+
+// using System.Collections;
+// using System.Collections.Generic;
+// //using System.Numerics; //commented out due to Vector2 conflicts with InputSystem pkg
+// using UnityEngine; 
+// using UnityEngine.InputSystem; 
+
+// public class PlayerController : MonoBehaviour
+// {
+//     //For movement; done by Matt 
+//     public float moveSpeed = 1f;
+//     public float collisionOffset = 0.05f;
+//     public ContactFilter2D movementFilter;
+//     public Vector2 movementInput;
+//     Rigidbody2D rb; //also used for shooting ammo
+//     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+
+//     //For Shooting hot dogs; done by K 
+//     public Weapon weapon; 
+//     Vector2 moveDirection; 
+//     Vector2 mousePosition; 
+//     //private bool isFiring = false; //Tracks whether firing is in progress 
+
+//     // Start is called before the first frame update
+//     void Start()
+//     {
+//         rb = GetComponent<Rigidbody2D>();
+//     }
+
+//     //K's section for shooting ammo 
+
+//     public bool isFiring = false; 
+
+//     public void StartFiring(){ 
+//         isFiring = true; 
+//         StartCoroutine(FireCoroutine()); 
+//     }
+
+//     public void StopFiring() { 
+//         isFiring = false; 
+//     }
+
+//     IEnumerator FireCoroutine() { 
+//         while (isFiring) { 
+//             weapon.Fire(); 
+//             yield return new WaitForSeconds(0.3f); //Adjust the delay between shots 
+//         }
+//     }
+//     void Update() { 
+//         float moveX = Input.GetAxisRaw("Horizontal"); 
+//         float moveY = Input.GetAxisRaw("Vertical"); 
+
+//         if(Input.GetKeyDown(KeyCode.Space)) {
+//             StartFiring(); 
+//         }
+//         else if (Input.GetKeyUp(KeyCode.Space)) { 
+//             StopFiring(); 
+//         }
+//         moveDirection = new Vector2(moveX, moveY).normalized; 
+//         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+//     }
+
+//     private void FixedUpdate() 
+//     {
+//         // if movement input is not 0, try to move - Matt
+//         if (movementInput != Vector2.zero) {
+//             int count = rb.Cast(movementInput, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+//             if (count == 0) {
+//                 rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+//             }
+//         }  
+
+//         // handle physics calcs for shooting - K 
+//         rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed); 
+//         Vector2 aimDirection = mousePosition - rb.position; //Calculating angle based on mouse pos
+//         float aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg - 90f; 
+//         rb.rotation = aimAngle; 
+//     }
+
+//     public void OnMove(InputValue movementValue) {
+//         movementInput = movementValue.Get<Vector2>();
+//     }
+// }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -1,32 +1,141 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using Playerspace;
 
 public class Weapon : MonoBehaviour
-{   //K's weapon class 
-    public GameObject bulletPrefab; 
-    public Transform firePoint; 
-    public float fireForce = 20f; 
+{
+    // Reference to the player controller
+    private PlayerController playerController;
 
-    public bool isFiring = false; 
+    //K's weapon class 
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float fireForce = 20f;
 
-    public void StartFiring(){ 
-        isFiring = true; 
-        StartCoroutine(FireCoroutine()); 
+    public bool isFiring = false;
+
+    public float bulletLifetime = 3f;
+
+    // Initialize the weapon with the player controller reference
+    public void Initialize(PlayerController controller)
+    {
+        playerController = controller;
     }
 
-    public void StopFiring() { 
-        isFiring = false; 
+    public void StartFiring()
+    {
+        isFiring = true;
+        playerController.StartCoroutine(FireCoroutine());
     }
-    public void Fire() { 
-        GameObject Bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); 
-        Bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse); 
-    } 
 
-    IEnumerator FireCoroutine() { 
-        while (isFiring) { 
-            Fire(); 
-            yield return new WaitForSeconds(0.1f); //Adjust the delay between shots 
+    public void StopFiring()
+    {
+        isFiring = false;
+    }
+
+    public void Fire()
+    {
+        GameObject bullet = GameObject.Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+        bulletRb.AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+
+        // Destroy bullet after a certain amount of time
+        GameObject.Destroy(bullet, bulletLifetime);
+    }
+
+    private IEnumerator FireCoroutine()
+    {
+        while (isFiring)
+        {
+            Fire();
+            yield return new WaitForSeconds(0.1f); // Adjust the delay between shots
         }
     }
 }
+
+
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Weapon : MonoBehaviour
+// {   //K's weapon class 
+//     public GameObject bulletPrefab; 
+//     public Transform firePoint; 
+//     public float fireForce = 20f; 
+
+//     public bool isFiring = false; 
+
+//     public float bulletLifetime = 3f; 
+//     public void StartFiring(){ 
+//         isFiring = true; 
+//         StartCoroutine(FireCoroutine()); 
+//     }
+
+//     public void StopFiring() { 
+//         isFiring = false; 
+//     }
+//     public void Fire() { 
+//         GameObject Bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation); 
+//         Bullet.GetComponent<Rigidbody2D>().AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+
+//         //Destroy bullet after a certain amount of time 
+//         Destroy(Bullet, bulletLifetime);  
+//     } 
+
+//     IEnumerator FireCoroutine() { 
+//         while (isFiring) { 
+//             Fire(); 
+//             yield return new WaitForSeconds(0.1f); //Adjust the delay between shots 
+//         }
+//     }
+// }
+
+
+// using System.Collections;
+// using UnityEngine;
+
+// namespace Weaponspace
+// {
+//     public class Weapon
+//     {
+//         //K's weapon class 
+//         public GameObject bulletPrefab;
+//         public Transform firePoint;
+//         public float fireForce = 20f;
+
+//         public bool isFiring = false;
+
+//         public float bulletLifetime = 3f;
+
+//         public void StartFiring(MonoBehaviour monoBehaviour)
+//         {
+//             isFiring = true;
+//             monoBehaviour.StartCoroutine(FireCoroutine());
+//         }
+
+//         public void StopFiring()
+//         {
+//             isFiring = false;
+//         }
+
+//         public void Fire()
+//         {
+//             GameObject bullet = GameObject.Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+//             Rigidbody2D bulletRb = bullet.GetComponent<Rigidbody2D>();
+//             bulletRb.AddForce(firePoint.up * fireForce, ForceMode2D.Impulse);
+
+//             // Destroy bullet after a certain amount of time
+//             GameObject.Destroy(bullet, bulletLifetime);
+//         }
+
+//         private IEnumerator FireCoroutine()
+//         {
+//             while (isFiring)
+//             {
+//                 Fire();
+//                 yield return new WaitForSeconds(0.1f); // Adjust the delay between shots
+//             }
+//         }
+//     }
+// }
